eb7351f0f7e9992f6cc7e3f574844ff56cdd1c3f
Khaled <kjalloul@anybotics.com>
2025-02-11 18:47:51 +0000

    Add ROS control to URDF

--- .devcontainer/install_ros2.bash
+++ .devcontainer/install_ros2.bash
@@ -1,12 +1,3 @@
-# locale  # check for UTF-8
-
-# apt update && apt install -y locales
-# locale-gen en_US en_US.UTF-8
-# update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
-# export LANG=en_US.UTF-8
-
-# locale  # verify settings
-
 curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
@@ -16,4 +7,9 @@ apt upgrade -y
 apt install -y software-properties-common
 add-apt-repository universe -y
 
-apt install -y ros-dev-tools ros-jazzy-desktop python3-colcon-clean
+apt install -y ros-dev-tools \
+    ros-jazzy-desktop \
+    python3-colcon-clean
+
+# Gazebo
+# ros-jazzy-ros-gz
---
+++ robot_description/launch/rviz_control.launch.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+
+RVIZ_LAUNCH_PATH = (
+    Path(get_package_share_directory("robot_description"))
+    / "launch" / "rviz.launch.py"
+)
+
+CONTROLLER_MANAGER_CONFIG = (
+    Path(get_package_share_directory("robot_description"))
+    / "config" / "controller_manager.yaml"
+)
+
+
+def generate_launch_description():
+    rviz_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(str(RVIZ_LAUNCH_PATH)),
+        launch_arguments={'use_joint_state_publisher_gui': 'false'}.items(),
+    )
+
+    ros2_control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        output="both",
+        parameters=[str(CONTROLLER_MANAGER_CONFIG)],
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+    )
+
+    velocity_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["velocity_controller",
+                   "--param-file", str(CONTROLLER_MANAGER_CONFIG)],
+    )
+
+    return LaunchDescription(
+        [
+            rviz_launch,
+            ros2_control_node,
+            joint_state_broadcaster_spawner,
+            velocity_controller_spawner,
+        ]
+    )
