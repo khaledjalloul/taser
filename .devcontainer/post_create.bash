d47252aaaeaa376cb3aebf6c1bc5980f7fc36c0f
Khaled Jalloul <khaled.jalloul@hotmail.com>
2025-05-30 07:33:00 +0000

    Fix Omniverse cache in docker

--- .devcontainer/post_create.bash
+++ .devcontainer/post_create.bash
@@ -4,6 +4,9 @@ kj setup cli-completion
 # Change ownership of the workspace to the devcontainer user
 sudo chown -R $(whoami) .
 
+# Change ownership of the omni cache used in Docker volume
+sudo chown -R $(whoami) /home/$(whoami)/venv/lib/python3.10/site-packages/omni/cache
+
 # Install new ROS dependencies in case of changes
 # rosdep update
 # rosdep install -iry --from-paths src
