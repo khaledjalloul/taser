fd6401fd01aebc393dc281417c409a7eb1772571
Khaled Jalloul <khaled.jalloul@hotmail.com>
2025-09-09 00:03:33 +0200

    Merge docker and devcontainer folders

--- .devcontainer/post_create.bash
+++ .devcontainer/post_create.bash
@@ -1,5 +1,5 @@
 # Set up CLI completion
-kj setup cli-completion
+kj setup cli completion
 
 # Change ownership of the workspace to the devcontainer user
 sudo chown -R $(whoami) .
@@ -15,7 +15,7 @@ sudo chown -R $(whoami) /home/$(whoami)/.local/lib/python3.10/site-packages/omni
 colcon build --packages-ignore taser_isaaclab
 
 # Add utils.bash to .bashrc
-echo 'source /home/$(whoami)/colcon_ws/.devcontainer/utils.bash' >> ~/.bashrc
+echo 'source /workspaces/taser_ws/.devcontainer/utils.bash' >> ~/.bashrc
 
 # Load environment variables
-echo 'source /home/$(whoami)/colcon_ws/.env.local' >> ~/.bashrc
+echo 'source /workspaces/taser_ws/.env.local' >> ~/.bashrc
