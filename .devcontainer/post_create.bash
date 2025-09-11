84939c64c2a6615c3d2a7b3c63bc503937574ba3
Khaled Jalloul <khaled.jalloul@hotmail.com>
2025-09-11 15:40:36 +0000

    Unify dockerfiles to use ROS Jazzy and Isaac Sim 5

--- .devcontainer/post_create.bash
+++ .devcontainer/post_create.bash
@@ -1,21 +1,15 @@
 # Set up CLI completion
 kj setup cli completion
 
-# Change ownership of the workspace to the devcontainer user
-sudo chown -R $(whoami) .
-
-# Change ownership of the omni cache used in Docker volume
-sudo chown -R $(whoami) /home/$(whoami)/.local/lib/python3.10/site-packages/omni/cache
-
-# Install new ROS dependencies in case of changes
-# rosdep update
-# rosdep install -iry --from-paths src
-
-# Build the project again in case of changes
-colcon build --packages-ignore taser_isaaclab
-
 # Add utils.bash to .bashrc
 echo 'source /workspaces/taser_ws/.devcontainer/utils.bash' >> ~/.bashrc
 
 # Load environment variables
 echo 'source /workspaces/taser_ws/.env.local' >> ~/.bashrc
+
+# Build the project
+source /opt/ros/jazzy/setup.sh && colcon build --packages-ignore taser_isaaclab
+/workspaces/isaacsim/python.sh -m pip install -e src/taser_isaaclab
+
+# Create URDF file
+source /opt/ros/jazzy/setup.sh && xacro /workspaces/taser_ws/src/taser_ros/urdf/robot.urdf.xacro -o /tmp/taser/robot.urdf
