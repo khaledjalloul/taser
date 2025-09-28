353f8f7919f3f8dcdac4085686ccad04b7e95dc1
Khaled Jalloul <khaled.jalloul@hotmail.com>
2025-09-28 19:47:59 +0000

    Add URDF importer

--- src/taser/isaacsim/robot.py
+++ src/taser/isaacsim/robot.py
@@ -1,18 +1,16 @@
 from pathlib import Path
 
 import numpy as np
+import omni.kit.commands
 from ament_index_python.packages import get_package_share_directory
+from isaacsim.asset.importer.urdf import _urdf
 from isaacsim.core.prims import Articulation
-from isaacsim.core.utils.stage import add_reference_to_stage
 from isaacsim.core.utils.types import ArticulationActions
 from omni.isaac.core.prims import XFormPrim
 
 from taser.isaacsim.utils.occupancy_grid import OccupancyGrid
 from taser.isaacsim.utils.ros2_tf_publisher import add_tf_publisher
 from taser.manipulation import IKManipulator
-
-NAME = "Taser"
-PRIM_PATH = f"/World/{NAME}"
 
 
 class TaserIsaacSimRobot(XFormPrim):
@@ -31,36 +29,61 @@ class TaserIsaacSimRobot(XFormPrim):
         self._position = np.array(position)
         self._orientation = np.array(orientation)
 
-        asset_path = (
-            Path(get_package_share_directory("taser_ros"))
-            / "robot_description"
-            / "usd"
-            / "taser.usd"
-        )
-        self._prim = add_reference_to_stage(
-            usd_path=str(asset_path.resolve()), prim_path=PRIM_PATH
-        )
+        self._import_urdf()
 
         super().__init__(
-            name=NAME,
-            prim_path=PRIM_PATH,
+            name="taser",
+            prim_path=self._prim_path,
             position=position,
             orientation=orientation,
         )
 
         add_tf_publisher(
-            robot_name=NAME,
-            target_prim=f"{PRIM_PATH}/base_wrapper",
+            robot_name="taser",
+            target_prim=f"{self._prim_path}/base_wrapper",
             tf_publisher_topic="/tf",
         )
 
-        self._articulation = Articulation(prim_paths_expr=PRIM_PATH, name="robot")
+        self._articulation = Articulation(prim_paths_expr=self._prim_path, name="taser")
 
         self._manipulator = IKManipulator()
 
     def step(self, dt: float, occupancy_grid: OccupancyGrid) -> None:
         action = ArticulationActions(
-            joint_velocities=np.zeros(len(self._articulation.dof_names)),
+            joint_velocities=np.ones(len(self._articulation.dof_names)) * 10,
             joint_names=self._articulation.dof_names,
         )
         self._articulation.apply_action(action)
+
+    def _import_urdf(self) -> None:
+        urdf_path = str(
+            Path(get_package_share_directory("taser_ros"))
+            / "robot_description"
+            / "urdf"
+            / "taser.urdf"
+        )
+
+        import_config = _urdf.ImportConfig()
+        import_config.convex_decomp = False
+        import_config.fix_base = False
+        import_config.make_default_prim = False
+        import_config.self_collision = True
+        import_config.distance_scale = 1
+        import_config.density = 0.0
+
+        # Parse the robot's URDF file to generate a robot model
+        result, robot_model = omni.kit.commands.execute(
+            "URDFParseFile", urdf_path=urdf_path, import_config=import_config
+        )
+
+        # Update the joint drive parameters for better stiffness and damping
+        for joint in robot_model.joints:
+            robot_model.joints[joint].drive.strength = 0.0
+            robot_model.joints[joint].drive.damping = 0.5
+
+        # Import the robot onto the current stage and retrieve its prim path
+        result, self._prim_path = omni.kit.commands.execute(
+            "URDFImportRobot",
+            urdf_robot=robot_model,
+            import_config=import_config,
+        )
